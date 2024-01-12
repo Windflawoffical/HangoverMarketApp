bcacfcb8ff540a53b5914b7f320e2e44610a021a
Kirill <kpicmo@mail.ru>
2024-01-12 14:57:04 +0300

    updated class alcohol

--- src/main/java/com/example/hangovermarketwebservice/Models/Alcohol.java
+++ src/main/java/com/example/hangovermarketwebservice/Models/Alcohol.java
@@ -1,7 +1,6 @@
 package com.example.hangovermarketwebservice.Models;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
 @Table(
@@ -9,9 +8,60 @@ import lombok.Data;
         schema = "public"
 )
 public class Alcohol {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
     @Column(name = "description", nullable = false)
     private String description;
+
+    @Column(name = "price", nullable = false)
+    private float price;
+
+    @Column(name = "manufacturer", nullable = false)
+    private String manufacturer;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
 }
