977aa5d569679a42a8c0b6b88bb478218e5e5321
Kirill "Windflaw" Valikov <93864873+Windflawoffical@users.noreply.github.com>
2024-01-12 15:29:21 +0300

    Add GetMapping

--- src/main/java/com/example/hangovermarketwebservice/Controllers/AlcoholController.java
+++ src/main/java/com/example/hangovermarketwebservice/Controllers/AlcoholController.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/alcohols")
 public class AlcoholController {
--- src/main/java/com/example/hangovermarketwebservice/Controllers/IndexController.java
+++ src/main/java/com/example/hangovermarketwebservice/Controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.hangovermarketwebservice.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class IndexController {
+
+    @GetMapping("/")
     public String hello() {
         return "Hello, alcash";
     }
